bbdb9bff7b02c2f06c54a7ad1d342dcf2688ea6e
Joshua White <56774271+joshuawhite389@users.noreply.github.com>
2024-09-08 20:37:06 -0400

    starting on post component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,16 @@
-
+import { useState } from 'react';
+import Post from './Post';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [posts, setPosts] = useState(['post1', 'post2', 'post3']);
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import '../styles/Post.css';
+
 
 const Post = () => {
   return (
-    <div>Post</div>
-  )
-}
+    <div>
+      <div className='postInfo'>
+        <FontAwesomeIcon className="avatar" icon={faUser} />
+        <div className='username'>Username</div>
+        <div className='spacerDots'>{'\u2B24'}</div>
+        <div className='timestamp'>Time/ Date of Post</div>
+        <div className='spacerDots'>{'\u2B24'}{'\u2B24'}{'\u2B24'}</div>
+      </div>
+      <div className="postTitle">Title of Post</div>
+      <div className="postContent">This is a new post with the following content...</div>
+    </div>
+  );
+};
 
-export default Post
+export default Post;
